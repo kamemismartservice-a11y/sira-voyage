0c253552d36eec01763736e2d042aeeae3b35fa0
Djorane Comara <votre-email@exemple.com>
2026-08-22 22:41:33 +0000

    Correction footer duplique sur la page d accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
@@ -102,8 +101,6 @@ export default async function Home() {
           ))}
         </div>
       </section>
-
-      <Footer />
     </main>
   );
 }
